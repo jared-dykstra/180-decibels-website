589f0851962450c6d0ba19ac16e5fdd5b35ca9be
Jared Dykstra <jared.dykstra@gmail.com>
2018-11-14 14:36:33 -0700

    Mobile device fixes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,11 +3,17 @@ import { Container, Navbar } from 'reactstrap'
 
 import styles from './Footer.module.scss'
 
-export default () => (
-  <Navbar color="faded" light className={`fixed-bottom ${styles.footer}`}>
+export default () => [
+  <div key="footer-spacing" className={styles.footerSpacing} />,
+  <Navbar
+    key="footer"
+    color="faded"
+    light
+    className={`fixed-bottom ${styles.footer}`}
+  >
     <Container>
       <a href="mailto:info@180decibels.com">info@180decibels.com</a>
       <a href="tel:+18883214531">1-888-321-4531</a>
     </Container>
   </Navbar>
-)
+]
