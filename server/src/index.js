f08c3654bf3aad79f2dfbe036ff948da69f0a651
Jared Dykstra <jared.dykstra@gmail.com>
2018-11-22 09:29:26 -0700

    Handles caching reasonably well

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const restify = require('restify')
+const favicon = require('serve-favicon')
 const compression = require('compression')
 const serveStatic = require('serve-static')
 const graphqlHTTP = require('express-graphql')
@@ -9,10 +10,35 @@ const apiSchema = require('./apiSchema')
 const server = ({ clientRoot }) => {
   const app = restify.createServer()
 
+  // Use gzip compression for static resources (files, etc)
   app.use(compression())
+  // use gzip compression for GraphQL API calls
+  app.use(restify.plugins.gzipResponse())
 
-  app.use(serveStatic(clientRoot, { immutable: true }))
+  app.use(favicon(path.join(clientRoot, 'favicon.ico')))
 
+  const isProduction = process.env.NODE_ENV === 'production'
+  const unhashedCacheDuration = isProduction ? 3600 : 0
+
+  // serve-static middleware for all files in the clientRoot directory
+  app.use(
+    serveStatic(clientRoot, {
+      index: false,
+      immutable: true,
+      maxAge: 31536000000, // <== serve-static accepts 'ms' and converts it to 's' for Cache-Control
+      setHeaders: (res, p) => {
+        // Limit caching for files which don't have a hash in their name
+        if (p.endsWith('manifest.json') || p.endsWith('index.html')) {
+          res.setHeader(
+            'Cache-Control',
+            `public, max-age=${unhashedCacheDuration}`
+          )
+        }
+      }
+    })
+  )
+
+  // GraphQL API
   app.post(
     '/api',
     graphqlHTTP({
@@ -20,7 +46,6 @@ const server = ({ clientRoot }) => {
       graphiql: false
     })
   )
-
   app.get(
     '/api',
     graphqlHTTP({
@@ -30,13 +55,16 @@ const server = ({ clientRoot }) => {
   )
 
   // Handles any requests that don't match the ones above, so react-router routes work
-  app.get('*', async (req, res, next) => {
-    try {
-      res.sendFile(path.join(clientRoot, `index.html`))
-    } catch (e) {
-      next(e)
-    }
-  })
+  // This includes the bare URL
+  app.get(
+    '*',
+    // TODO: restify.plugins.serveStatic is not generating an ETag header
+    restify.plugins.serveStatic({
+      directory: clientRoot,
+      file: 'index.html',
+      maxAge: unhashedCacheDuration
+    })
+  )
 
   const port = process.env.PORT || 5000
   app.listen(port)
