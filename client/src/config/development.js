f2ea957ecace903ddb38a3876073412f897d3e34
Jared Dykstra <jared.dykstra@gmail.com>
2018-11-23 17:08:31 -0700

    simple client library & fetch

--- client/src/config/development.js
+++ client/src/config/development.js
@@ -1,5 +1,3 @@
 // WARNING: All values will end up in the client-side bundle, regardless of NODE_ENV
 
-export default {
-  apiEndpoint: 'http://localhost:5000/graphql'
-}
+export default {}
--- client/src/redux/userManagement/fetcher.js
+++ client/src/redux/userManagement/fetcher.js
@@ -1,20 +1,33 @@
+import { createApolloFetch } from 'apollo-fetch'
+
 import { get as configGet } from 'config'
 
 import { REGISTER_FORM_EMAIL_KEY } from './userManagementConstants'
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
+const uri = configGet('apiEndpoint')
+const fetcher = createApolloFetch({ uri })
+
 // If thrown errors are wrapped with `new Error()`,  the payload isn't available via redux-saga
 // see: https://github.com/erikras/redux-form/issues/2442
 /* eslint-disable no-throw-literal */
 
 export const registerUser = async payload => {
-  const { user } = payload
-  console.log(`TODO: Register user=${JSON.stringify(user)}`)
-  console.log(`API Endpoint=${configGet('apiEndpoint')}`)
-  await sleep(2 * 1000)
-  throw {
-    [REGISTER_FORM_EMAIL_KEY]:
-      'That email address is already registered, please login'
+  try {
+    const { user } = payload
+    console.log(`TODO: Register user=${JSON.stringify(user)}`)
+
+    const retval = await fetcher({ query: { hello: '' } })
+    console.log(`retval=${retval}`)
+
+    await sleep(2 * 1000)
+    throw {
+      [REGISTER_FORM_EMAIL_KEY]:
+        'That email address is already registered, please login'
+    }
+  } catch (err) {
+    console.error(err)
+    throw err
   }
 }
