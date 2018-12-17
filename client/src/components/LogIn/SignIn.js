b59804300c583aa13dcc11c6932df1f1bfe1362a
Jared Dykstra <jared.dykstra@gmail.com>
2018-12-17 12:11:51 -0700

    Makes the password field a reusable component

--- client/src/components/LogIn/SignIn.js
+++ client/src/components/LogIn/SignIn.js
@@ -3,10 +3,7 @@ import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Field, reduxForm, propTypes } from 'redux-form/immutable'
-import InputAdornment from '@material-ui/core/InputAdornment'
-import IconButton from '@material-ui/core/IconButton'
-import Visibility from '@material-ui/icons/Visibility'
-import VisibilityOff from '@material-ui/icons/VisibilityOff'
+
 import FormGroup from '@material-ui/core/FormGroup'
 
 import {
@@ -21,6 +18,7 @@ import { SIGNIN_FORM_KEY } from 'reduxStore/auth/authConstants'
 import styles from './LogIn.module.scss'
 import renderField from './renderField'
 import Buttons from './Buttons'
+import PasswordField from './PasswordField'
 
 class SignIn extends PureComponent {
   static propTypes = {
@@ -31,25 +29,21 @@ class SignIn extends PureComponent {
     ...propTypes
   }
 
-  initialState = Immutable.from({
-    showPassword: false
-  })
-
   constructor(props) {
     super(props)
-    this.state = this.initialState
-  }
-
-  handleClickShowPassword = () => {
-    this.setState(state =>
-      Immutable.from({ showPassword: !state.showPassword })
-    )
+    this.state = Immutable.from({
+      resetTs: new Date().getTime()
+    })
   }
 
   handleClickReset = args => {
     const { reset } = this.props
     reset(args)
-    this.setState(state => this.initialState)
+    this.setState(() =>
+      Immutable.from({
+        resetTs: new Date().getTime()
+      })
+    )
   }
 
   render() {
@@ -62,8 +56,9 @@ class SignIn extends PureComponent {
       cancelLabel,
       resetLabel
     } = this.props
-    const { showPassword } = this.state
-
+    // Whenever resetTs is changed, react elements using that value of a key are re-rendered, and their internal state is reset
+    // This resets the "show password" functionality when the form is reset
+    const { resetTs } = this.state
     const isSubmitDisabled = submitting
     const isResetDisabled = pristine || submitting
     return (
@@ -80,26 +75,14 @@ class SignIn extends PureComponent {
             fullWidth
           />
         </FormGroup>
-        <FormGroup>
-          <Field
+        <FormGroup row>
+          <PasswordField
+            key={resetTs}
             label="Password"
-            id={SIGNIN_FORM_PASSWORD_KEY}
-            name={SIGNIN_FORM_PASSWORD_KEY}
-            type={showPassword ? 'text' : 'password'}
-            component={renderField}
+            formKey={SIGNIN_FORM_PASSWORD_KEY}
             placeholder="password"
             autoComplete="current-password"
             fullWidth
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="Toggle password visibility"
-                  onClick={this.handleClickShowPassword}
-                >
-                  {showPassword ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            }
           />
         </FormGroup>
         <FormGroup>
