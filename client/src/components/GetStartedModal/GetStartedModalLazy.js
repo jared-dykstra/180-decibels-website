39fced3db012f5f5c904180a6dd58849858de154
Jared Dykstra <jared.dykstra@gmail.com>
2019-01-07 22:00:28 -0700

    Proper handling of children prop

--- client/src/components/GetStartedModal/GetStartedModalLazy.js
+++ client/src/components/GetStartedModal/GetStartedModalLazy.js
@@ -5,7 +5,7 @@ const GetStartedModal = React.lazy(async () => import('./GetStartedModal'))
 const GetStartedModalLazy = ({ children, ...props }) => (
   // Fallback isn't going to be visible.  If the user clicks the button to open the modal, redux state updates and the modal will open as soon as it loads
   <Suspense fallback={<div>Loading...</div>}>
-    <GetStartedModal {...props} />
+    <GetStartedModal {...props}>{children}</GetStartedModal>
   </Suspense>
 )
 
