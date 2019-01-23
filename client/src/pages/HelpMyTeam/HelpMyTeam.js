bfacc82cae14bddee033acca463aa5d9963f32fe
Jared Dykstra <jared.dykstra@gmail.com>
2019-01-22 20:48:21 -0700

    Typography slacked to me by Kerri

--- client/src/pages/HelpMyTeam/HelpMyTeam.js
+++ client/src/pages/HelpMyTeam/HelpMyTeam.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { Paper, Typography } from '@material-ui/core'
 import { withStyles } from '@material-ui/core/styles'
 
-import { SelfAssessment, Template } from 'components'
+import { SelfAssessment, Quote, Template } from 'components'
 
 import styles from './HelpMyTeam.module.scss'
 import pageStyles from '../pageStyles'
@@ -19,6 +19,9 @@ const styles2 = theme => ({
   },
   intro: {
     marginBottom: theme.spacing.unit * 7
+  },
+  quote: {
+    margin: theme.spacing.unit * 10
   }
 })
 
@@ -27,15 +30,37 @@ const renderFullPage = props => {
   return (
     <Paper className={`${classes.paper} ${classes.fullHeight}`}>
       <h1>Self Assessment</h1>
+
       <Typography variant="body1">
         It only takes one minute to get results you can use.
       </Typography>
-      <Typography variant="body1" className={classes.intro}>
+      <Typography variant="body1">
         Use this self-assessment to understand your situation. These questions
         are enough to create an initial report, which will contain concrete,
         actionable steps that you can immediately use.
       </Typography>
       {renderQuiz(props)}
+      <Typography variant="body1">
+        We generate real ROI and substantially grow confidence team&apos;s
+        ability to execute.
+      </Typography>
+      <Typography variant="body1" className={classes.intro}>
+        We let you set the direction while we saturate your organization with a
+        proven process for getting where you want to go. Our mission is to
+        measurably improve team productivity with tactical operational tools and
+        processes.
+      </Typography>
+      <Quote
+        right
+        cite="John, Executive Director"
+        className={`h4 ${classes.quote}`}
+      >
+        My very first sit down with 180 Decibels was eye opening, to say the
+        least. From the first few questions I knew that I had much to discover
+        and was about to enter a learning cycle that would take me to a great
+        future. I am discovering new aspects of myself and how I can cooperate
+        and lead my team.
+      </Quote>
     </Paper>
   )
 }
