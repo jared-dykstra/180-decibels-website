4d6bdf88014b75b4929522a5b27da25be015f9a8
Jared Dykstra <jared.dykstra@gmail.com>
2019-01-30 14:23:10 -0700

    GUIDs for IDs

--- client/src/reduxStore/vast/vastInitialState.js
+++ client/src/reduxStore/vast/vastInitialState.js
@@ -76,37 +76,57 @@ export default {
   model: Immutable.from({
     nodes: [
       {
-        id: '1',
+        id: 'fae3763d-2cb9-4d54-9132-e34bc14f45f2',
         label: 'Person A',
         type: NODE_TYPE_PERSON
       },
       {
-        id: '2',
+        id: '9a5cbd26-98d5-4057-977a-88f6d776542c',
         label: 'Person B',
         type: NODE_TYPE_PERSON
       },
       {
-        id: '3',
+        id: 'f00da641-6da9-4688-8763-9c4dac9d37eb',
         label: 'Accountability 1',
         type: NODE_TYPE_ACCOUNTABILITY
       },
       {
-        id: '4',
+        id: 'c8566db9-bf1f-45b0-b968-d79f22ce76c2',
         label: 'Accountability 2',
         type: NODE_TYPE_ACCOUNTABILITY
       },
       {
-        id: '5',
+        id: '95c54d46-0f7b-49ba-96cb-e4a5b96734fc',
         label: 'Accountability 3',
         type: NODE_TYPE_ACCOUNTABILITY
       }
     ],
     edges: [
-      { id: 'e13', source: '1', target: '3' },
-      { id: 'e12', source: '1', target: '2' },
-      { id: 'e24', source: '2', target: '4' },
-      { id: 'e15', source: '1', target: '5' },
-      { id: 'e25', source: '2', target: '5' }
+      {
+        id: '9a66d59d-65a3-4c78-8e97-a81c207942aa', // 'A1',
+        source: 'fae3763d-2cb9-4d54-9132-e34bc14f45f2',
+        target: 'f00da641-6da9-4688-8763-9c4dac9d37eb'
+      },
+      {
+        id: 'a47148ed-4ca9-49ed-81c0-5b913ba529ed', // 'AB',
+        source: 'fae3763d-2cb9-4d54-9132-e34bc14f45f2',
+        target: '9a5cbd26-98d5-4057-977a-88f6d776542c'
+      },
+      {
+        id: 'a4765819-018a-4e5e-b632-7a691e98993f', // 'B2',
+        source: '9a5cbd26-98d5-4057-977a-88f6d776542c',
+        target: 'c8566db9-bf1f-45b0-b968-d79f22ce76c2'
+      },
+      {
+        id: '6a94e60a-9c0e-495f-8133-20384bd699b3', // 'A3',
+        source: 'fae3763d-2cb9-4d54-9132-e34bc14f45f2',
+        target: '95c54d46-0f7b-49ba-96cb-e4a5b96734fc'
+      },
+      {
+        id: '69ff6993-355c-4547-be54-57e58a133770', // 'B3',
+        source: '9a5cbd26-98d5-4057-977a-88f6d776542c',
+        target: '95c54d46-0f7b-49ba-96cb-e4a5b96734fc'
+      }
     ]
   }),
 
