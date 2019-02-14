27214629076b7bd81564247be45b2105964c698e
Jared Dykstra <jared.dykstra@gmail.com>
2019-02-14 08:50:07 -0700

    Removed unnecessary styling

--- client/src/pages/Vast/AddNewView.js
+++ client/src/pages/Vast/AddNewView.js
@@ -78,13 +78,9 @@ const renderSuggestion = (suggestion, { query, isHighlighted }) => {
       <div>
         {parts.map((part, index) =>
           part.highlight ? (
-            <span key={String(index)} style={{ fontWeight: 800 }}>
-              {part.text}
-            </span>
+            <strong key={String(index)}>{part.text}</strong>
           ) : (
-            <strong key={String(index)} style={{ fontWeight: 300 }}>
-              {part.text}
-            </strong>
+            <span key={String(index)}>{part.text}</span>
           )
         )}
       </div>
